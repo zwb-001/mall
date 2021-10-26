efd5634c3601a22bf7f0aff447c9e865f29040f1
30812 <3081291537@qq.com>
2021-10-26 17:15:55 +0800

    submit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <router-view/>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
 
 
+import MainTabBar from "./components/cotent/mainTabBar/MainTabBar";
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
