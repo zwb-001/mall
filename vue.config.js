c6423f0361222be592cb89cd6f0aac17870fadd9
30812 <3081291537@qq.com>
2021-10-27 17:25:33 +0800

    submit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+module.exports = {devServer: {
+    host: '0.0.0.0',
+    public: '192.192.192.121:8080',//该网络地址为你联网的ip地址
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    disableHostCheck: true,
+  },
   configureWebpack: {
     resolve: {
       alias: {//别名
