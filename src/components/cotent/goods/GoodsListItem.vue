a12c5e7f96bb2246338c1d7bdc29f609906da8e8
30812 <3081291537@qq.com>
2021-11-01 15:50:05 +0800

    submit

--- src/components/cotent/goods/GoodsListItem.vue
+++ src/components/cotent/goods/GoodsListItem.vue
@@ -68,6 +68,6 @@ export default {
   top: -1px;
   width: 14px;
   height: 14px;
-  background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
+  background: url("../../../assets/img/common/collect.svg") 0 0/14px 14px;
 }
 </style>
